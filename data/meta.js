bb8f8335b52e681b3dcb95d95b067d7bd3b65c40
Andi Lanuza <andi@photokitchenfood.com>
2026-09-10 20:32:09 +0800

    All Colors Data

--- data/meta.js
+++ data/meta.js
@@ -1,8 +1,29 @@
 const CATALOG_META = {
   "lastUpdated": "2026-09-10",
   "colors": {
+    "beige-brown": {
+      "count": 134
+    },
     "red": {
       "count": 98
+    },
+    "white": {
+      "count": 58
+    },
+    "green": {
+      "count": 95
+    },
+    "pink-purple": {
+      "count": 49
+    },
+    "blue": {
+      "count": 114
+    },
+    "gray-black": {
+      "count": 78
+    },
+    "yellow-orange": {
+      "count": 74
     }
   }
 };
